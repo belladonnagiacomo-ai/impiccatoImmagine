5061b53253fd799409ba24286a022767639625a5
belladonna.giacomo <belladonna.giacomo@HP-006>
2026-05-14 09:59:44 +0200

    mod

--- impiccatoImmagine/Form1.Designer.cs
+++ impiccatoImmagine/Form1.Designer.cs
@@ -33,6 +33,13 @@
             button2 = new Button();
             button3 = new Button();
             label2 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -41,21 +48,23 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Showcard Gothic", 24F, FontStyle.Italic, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Black;
-            label1.Location = new Point(463, 34);
-            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Location = new Point(761, 42);
+            label1.Margin = new Padding(8, 0, 8, 0);
             label1.Name = "label1";
-            label1.Size = new Size(205, 40);
+            label1.Size = new Size(256, 50);
             label1.TabIndex = 0;
             label1.Text = " categoria ";
             // 
             // button1
             // 
             button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Font = new Font("Stencil", 18F, FontStyle.Italic);
             button1.ForeColor = Color.Black;
             button1.ImageAlign = ContentAlignment.TopLeft;
-            button1.Location = new Point(163, 155);
+            button1.Location = new Point(223, 193);
+            button1.Margin = new Padding(5, 4, 5, 4);
             button1.Name = "button1";
-            button1.Size = new Size(213, 46);
+            button1.Size = new Size(411, 56);
             button1.TabIndex = 1;
             button1.Text = "categoria animali";
             button1.UseVisualStyleBackColor = true;
@@ -63,9 +72,11 @@
             // 
             // button2
             // 
-            button2.Location = new Point(463, 155);
+            button2.Font = new Font("Stencil", 18F, FontStyle.Italic);
+            button2.Location = new Point(644, 191);
+            button2.Margin = new Padding(5, 4, 5, 4);
             button2.Name = "button2";
-            button2.Size = new Size(213, 46);
+            button2.Size = new Size(350, 56);
             button2.TabIndex = 2;
             button2.Text = "categoria paesi";
             button2.UseVisualStyleBackColor = true;
@@ -73,9 +84,11 @@
             // 
             // button3
             // 
-            button3.Location = new Point(745, 155);
+            button3.Font = new Font("Stencil", 18F, FontStyle.Italic);
+            button3.Location = new Point(1004, 189);
+            button3.Margin = new Padding(5, 4, 5, 4);
             button3.Name = "button3";
-            button3.Size = new Size(213, 46);
+            button3.Size = new Size(411, 56);
             button3.TabIndex = 3;
             button3.Text = "categoria tecnologia";
             button3.UseVisualStyleBackColor = true;
@@ -85,24 +98,110 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Showcard Gothic", 24F, FontStyle.Italic);
-            label2.Location = new Point(382, 34);
+            label2.Location = new Point(624, 42);
+            label2.Margin = new Padding(5, 0, 5, 0);
             label2.Name = "label2";
-            label2.Size = new Size(351, 40);
+            label2.Size = new Size(437, 50);
             label2.TabIndex = 4;
             label2.Text = "Scegli la difficoltà";
             // 
+            // button4
+            // 
+            button4.BackgroundImageLayout = ImageLayout.Center;
+            button4.ForeColor = Color.Black;
+            button4.ImageAlign = ContentAlignment.TopLeft;
+            button4.Location = new Point(223, 191);
+            button4.Margin = new Padding(5, 4, 5, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(411, 56);
+            button4.TabIndex = 5;
+            button4.Text = "facile ";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackgroundImageLayout = ImageLayout.Center;
+            button5.ForeColor = Color.Black;
+            button5.ImageAlign = ContentAlignment.TopLeft;
+            button5.Location = new Point(644, 193);
+            button5.Margin = new Padding(5, 4, 5, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(350, 56);
+            button5.TabIndex = 6;
+            button5.Text = "medio";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackgroundImageLayout = ImageLayout.Center;
+            button6.ForeColor = Color.Black;
+            button6.ImageAlign = ContentAlignment.TopLeft;
+            button6.Location = new Point(1004, 189);
+            button6.Margin = new Padding(5, 4, 5, 4);
+            button6.Name = "button6";
+            button6.Size = new Size(411, 56);
+            button6.TabIndex = 7;
+            button6.Text = "difficile";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(223, 594);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(65, 51);
+            textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Swis721 BT", 24F, FontStyle.Italic | FontStyle.Underline);
+            label3.Location = new Point(223, 517);
+            label3.Name = "label3";
+            label3.Size = new Size(142, 48);
+            label3.TabIndex = 9;
+            label3.Text = "Lettera";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(896, 594);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(472, 51);
+            textBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Swis721 BT", 24F, FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(896, 529);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 48);
+            label4.TabIndex = 11;
+            label4.Text = "Parola";
+            // 
             // form1
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(23F, 44F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1444, 881);
+            ClientSize = new Size(1844, 988);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
             Controls.Add(label2);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label1);
-            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(5, 6, 5, 6);
+            Font = new Font("Stencil", 22.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            Margin = new Padding(8, 7, 8, 7);
             Name = "form1";
             Text = "tecno";
             ResumeLayout(false);
@@ -116,5 +215,12 @@
         private Button button2;
         private Button button3;
         private Label label2;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
     }
 }
--- impiccatoImmagine/Form1.cs
+++ impiccatoImmagine/Form1.cs
@@ -2,7 +2,9 @@ namespace impiccatoImmagine
 {
     public partial class form1 : Form
     {
-        string riga;
+        string riga, nascosta;
+        string[] rigaSPlit;
+        int a = 0, t = 0, p = 0;
 
         private void menu()
         {
@@ -10,16 +12,49 @@ namespace impiccatoImmagine
             button2.Visible = false;
             button3.Visible = false;
             label1.Visible = false;
-            label2.Visible = false;
         }
+
+        private void Difficoltà()
+        {
+            label2.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+        }
+
+        private void Elimina()
+        {
+            label2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+        }
+
+        private void Crea()
+        {
+            label3.Visible = true;
+            label4.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+        }
+
         public form1()
         {
             InitializeComponent();
+            label2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
+
             Random r = new Random();
             List<string> animali = new List<string>(File.ReadLines("animali.csv"));
             animali.RemoveAt(0);
@@ -28,7 +63,8 @@ namespace impiccatoImmagine
             riga = animali[num];
 
             menu();
-            label2.Visible = true;
+            Difficoltà();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -41,6 +77,8 @@ namespace impiccatoImmagine
             riga = paesi[num];
 
             menu();
+            Difficoltà();
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -53,6 +91,35 @@ namespace impiccatoImmagine
             riga = tecno[num];
 
             menu();
+            Difficoltà();
+
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Elimina();
+            Crea();
+            rigaSPlit = riga.Split(",");
+            nascosta = rigaSPlit[0];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Elimina();
+            Crea();
+            rigaSPlit = riga.Split(",");
+            nascosta = rigaSPlit[1];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Elimina();
+            Crea();
+            rigaSPlit = riga.Split(",");
+            nascosta = rigaSPlit[2];
+        }
+
+
     }
 }
